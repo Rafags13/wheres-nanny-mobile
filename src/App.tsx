b1a3338b6fd3e14b28c45330a1d8102457fe4671
Rafael Veiga <rafael_todescato@hotmail.com>
2023-05-30 10:50:44 -0400

    fix: changing main source paste name 'app' to 'src'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import ModalProvider from './context/ModalContext';
 import Routes from './routes';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,11 @@ import StackNavigator from "./StackNavigator";
 import Modal from 'react-native-modal';
 import { Text, View } from "react-native";
 import Button from "../components/Button";
-import Lottie from 'lottie-react-native';
 import CurrentModal from "../components/CurrentModal";
 
 const queryClient = new QueryClient();
 
 export default function Routes() {
-    const { isVisible, closeModal } = useContext(ModalContext) as ModalContextType;
-
     return (
         <QueryClientProvider client={queryClient}>
             <CurrentModal />
