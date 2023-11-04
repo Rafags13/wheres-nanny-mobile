a534897bb66b60023f7f62abb9be020a7c23f364
Rafael Veiga <rafael_todescato@hotmail.com>
2023-11-04 19:16:00 -0400

    <fix>: finally solved the problem with development crashes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import 'react-native-reanimated'
-import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AppProvider } from './context';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <AppProvider>
-          <Routes />
-        </AppProvider>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+    </NavigationContainer>
   );
 }
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,6 @@
-import BottomSheet, { BottomSheetBackdrop, BottomSheetScrollView } from "@gorhom/bottom-sheet";
+// import BottomSheet, { BottomSheetBackdrop, BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { ReactNode, useCallback, useMemo } from "react";
+import { View } from 'react-native';
 import styles from "./style";
 
 type Props = {
@@ -7,27 +8,32 @@ type Props = {
 };
 
 export default function Modal({ children }: Props) {
-    const renderBackdrop = useCallback((props: any) => (
-        <BottomSheetBackdrop
-            {...props}
-            disappearsOnIndex={0}
-            appearsOnIndex={1}
-        />
-    ),
-        []
-    );
-
-    const snapPoints = useMemo(() => ['20%', '70%'], []);
-
     return (
-        <BottomSheet
-            index={0}
-            snapPoints={snapPoints}
-            backdropComponent={renderBackdrop}
-        >
-            <BottomSheetScrollView contentContainerStyle={styles.modalContainer} showsVerticalScrollIndicator={false}>
-                {children}
-            </BottomSheetScrollView>
-        </BottomSheet>
+        <View>
+            {children}
+        </View>
     )
+    // const renderBackdrop = useCallback((props: any) => (
+    //     <BottomSheetBackdrop
+    //         {...props}
+    //         disappearsOnIndex={0}
+    //         appearsOnIndex={1}
+    //     />
+    // ),
+    //     []
+    // );
+
+    // const snapPoints = useMemo(() => ['20%', '70%'], []);
+
+    // return (
+    //     <BottomSheet
+    //         index={0}
+    //         snapPoints={snapPoints}
+    //         backdropComponent={renderBackdrop}
+    //     >
+    //         <BottomSheetScrollView contentContainerStyle={styles.modalContainer} showsVerticalScrollIndicator={false}>
+    //             {children}
+    //         </BottomSheetScrollView>
+    //     </BottomSheet>
+    // )
 }
